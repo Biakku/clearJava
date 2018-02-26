03feed5b7695b761cbe99358d8c7093898fa52de
Biakku <36861699+Biakku@users.noreply.github.com>
2018-02-26 20:38:24 +0100

    Add files via upload

---
+++ Learning/src/Test.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+
+	public static void main(String[] args) {
+		
+		int pierdyliard = 1124;
+		float pierdyliardatko = 11.22f;
+		float suma = pierdyliard + pierdyliardatko;
+		
+		System.out.println(suma);
+		
+	}
+
+}
